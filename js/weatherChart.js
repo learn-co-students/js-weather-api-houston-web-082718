3a3b6750a1f8606edd94188a8826615fad9add21
gwatson86 <gwatson86@gmail.com>
2018-10-12 09:17:06 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,56 @@
-function getFahrenheits(result){
-  // Your code goes here
+function getFahrenheits(data) {
+  return data.map(function(temp) {
+    return temp.temperature;
+  })
 }
 
-function getHours(result){
-  // Your code goes here
+function getHours(data){
+  return data.map(function(hour) {
+    return new Date(hour.time * 1000).getHours();
+  })
 }
 
-function generateDataSet(labels, data) {
-  // Your code goes here
+function getHumidity(data) {
+  return data.map(function(humid) {
+    return humid.humidity * 100;
+  })
 }
 
-function makeRequest(endpoint, success) {
-  // Your code goes here
+function generateDataSet(times, temps, humids) {
+  return {
+    type: 'line',
+    data: {
+      labels: times,
+      datasets: [{
+        data: temps,
+        label: "Degrees Fahrenheit",
+        borderColor: "#FF0000"
+      },
+
+      {
+        data: humids,
+        label: "% Humidity",
+        borderColor: "#0000FF"
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Houston Forecast (Hourly)'
+      }
+    }
+  }
+}
+
+function makeRequest(endpoint, canvas) {
+  return fetch(endpoint)
+  .then(resp => resp.json())
+  .then(json => {
+        let hourlyData = json.hourly.data.map(({temperature, time, humidity}) => ({temperature, time, humidity}))
+        let temps = getFahrenheits(hourlyData);
+        let hours = getHours(hourlyData);
+        let humids = getHumidity(hourlyData);
+        let data = generateDataSet(hours, temps, humids);
+        // const tempChart = new Chart(canvas, data);
+        })
 }
